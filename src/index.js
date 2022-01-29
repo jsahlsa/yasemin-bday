957f2ed5b7430800f2787639d7e58f0b98087f83
jsahlsa <joe.sahlstrom@gmail.com>
2022-01-29 04:31:56 +0000

    ✨ pretty much done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-window.onload = function () {
-  document.getElementById('my_audio').play();
-};
+const playPauseButton = document.querySelector('button');
+const audio = document.querySelector('#my_audio');
+
+let playing = false;
+
+function handleToggle() {
+  if (!playing) {
+    audio.play();
+    playPauseButton.setAttribute('class', 'playing');
+    playing = true;
+  } else {
+    audio.pause();
+    audio.currentTime = 0;
+    playPauseButton.removeAttribute('class', 'playing');
+    playing = false;
+  }
+}
+
+playPauseButton.addEventListener('click', handleToggle);
